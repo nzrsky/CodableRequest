2e97a1ec441d351a137a68f5ee4692ba8defeffd
Alex Nazarov <nazarov@ptr.vc>
2024-05-26 23:06:25 +0300

    Use failing logger by default

--- Sources/CodableURLSession/CodableURLSession.swift
+++ Sources/CodableURLSession/CodableURLSession.swift
@@ -369,7 +369,7 @@ public class DefaultResponseTopLevelDecoder: AnyTopLevelDecoder {
     required public init() {}
 
     public func decode<T>(_ type: T.Type, from: (data: Data, response: HTTPURLResponse)) throws -> T where T: Decodable {
-        let decoder = ResponseDecoding<LoggingJSONDecoder.Provider>(response: from.response, data: from.data)
+        let decoder = ResponseDecoding<FailingJSONDecoder.Provider>(response: from.response, data: from.data)
         return try T(from: decoder)
     }
 }
